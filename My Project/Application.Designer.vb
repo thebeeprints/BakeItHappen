57dcae5999c0e9f70851ab1062a922989b12fff7
James Pandio <114295563+OneMapleLeaf@users.noreply.github.com>
2024-04-23 13:45:54 +0800

    renamed reference from Form1 to LoadingScreen

--- My Project/Application.Designer.vb
+++ My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
 
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()> _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.BakeItHappen.Form1
+            Me.MainForm = Global.BakeItHappen.LoadingScreen
         End Sub
     End Class
 End Namespace
